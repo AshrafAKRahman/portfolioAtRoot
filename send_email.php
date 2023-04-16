20b4e76a1b00eac66878abf0ed09abc20f8c8eaf
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-04-16 16:23:42 +0100

    api and email now in correct file, also composer now installed

--- send_email.php
+++ send_email.php
@@ -1,53 +1,44 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 require 'vendor/autoload.php';
 
-header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Methods");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-if (!empty($data['firstName']) && !empty($data['lastName']) && !empty($data['email']) && !empty($data['phone']) && !empty($data['message'])) {
-    $firstName = $data['firstName'];
-    $lastName = $data['lastName'];
-    $email = $data['email'];
-    $phone = $data['phone'];
-    $message = $data['message'];
-    
-    $to = "ashraf.ak.rahman@otlook.com";
-    $subject = "Contact Form Submission";
-    $body = "Name: {$firstName} {$lastName}\nEmail: {$email}\nPhone: {$phone}\n\nMessage:\n{$message}";
-
-    $mail = new PHPMailer(true);
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'smtp.sendgrid.net';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'apikey';
-        $mail->Password = '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';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
-
-        $mail->setFrom($email, "{$firstName} {$lastName}");
-        $mail->addAddress($to);
-        $mail->addReplyTo($email);
-
-        $mail->isHTML(false);
-        $mail->Subject = $subject;
-        $mail->Body = $body;
-
-        $mail->send();
-        echo json_encode(["message" => "Message sent successfully."]);
-    } catch (Exception $e) {
-        http_response_code(500);
-        echo json_encode(["message" => "Error sending message: {$mail->ErrorInfo}"]);
-    }
-} else {
+if (empty($data['firstName']) || empty($data['lastName']) || empty($data['email']) || empty($data['phone']) || empty($data['message'])) {
     http_response_code(400);
-    echo json_encode(["message" => "Please fill in all fields."]);
+    echo json_encode(['error' => 'All fields are required']);
+    exit;
 }
-?>
+
+$firstName = $data['firstName'];
+$lastName = $data['lastName'];
+$email = $data['email'];
+$phone = $data['phone'];
+$message = $data['message'];
+
+$to = getenv('EMAIL_TO');
+$subject = 'New contact form submission';
+$body = "You have received a new message from your contact form.\n\n" .
+    "Name: $firstName $lastName\n" .
+    "Email: $email\n" .
+    "Phone: $phone\n" .
+    "Message: $message";
+
+$headers = "From: $email" . "\r\n" .
+    "Reply-To: $email" . "\r\n" .
+    "X-Mailer: PHP/" . phpversion();
+
+if (mail($to, $subject, $body, $headers)) {
+    http_response_code(200);
+    echo json_encode(['message' => 'Message sent successfully']);
+} else {
+    http_response_code(500);
+    echo json_encode(['error' => 'Message could not be sent']);
+}
